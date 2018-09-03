9dcc67c5742956d8bb08bffb42277849bb98beff
erifranck <erifranckjose@gmail.com>
2018-09-02 21:44:39 -0500

    insert task board

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
-import {Paper} from 'components'
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import {TaskBoard} from 'components'
 
+const DIV = styled.div`
+  min-height: 800px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .board {
+    margin-left: 100px;
+  }
+`
 class App extends Component {
   render() {
     return (
-      <Paper>
-         asodkadokadosk
-      </Paper>
+      <DIV>
+        <TaskBoard tasks={["task1", "task2", "task3"]} title='Todo' className='board' />
+        <TaskBoard tasks={[]} title='In Progress' className='board' />
+        <TaskBoard tasks={[]} title='Done' className='board' />
+      </DIV>
     );
   }
 }
